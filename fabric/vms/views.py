f7aef5e678f724a25cdb3c3d6f5747c19de8d6e1
AdzLovelace <heyinyu.sensi@foxmail.com>
2023-04-23 20:24:03 +0800

    删除了原本用于测试得代码

--- fabric/vms/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-import asyncio
-
-
-async def tcp_view(request):
-    host = 'localhost'
-    port = 8888
-    client_socket = await asyncio.open_connection(host, port)
